24f4c5f77f6fd7127931dbb794b9ccc2f818c67c
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-09-22 00:17:07 +0600

    Added hash function

--- Check-Hash/app.go
+++ Check-Hash/app.go
@@ -5,10 +5,12 @@ import (
 	"os"
 	"log"
 	"bufio"
+	"crypto/sha512"
+	"strings"
 )
 
 func readFile() string {
-	var message string;
+	var builder strings.Builder;
 	file, err := os.Open("files/one-text.txt");
 	if err != nil {
 		log.Fatal(err);
@@ -17,17 +19,20 @@ func readFile() string {
 
 	scanner := bufio.NewScanner(file);
 	for scanner.Scan() {
-		message += scanner.Text() + "\n";
+		builder.WriteString(scanner.Text() + "\n");
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err);
 	}
-	return message;
+	return builder.String();
 }
 
 func main() {
 	var message string = readFile();
+	byteMessage := []byte(message);
+	hash := sha512.New();
+	hash.Write(byteMessage)
 
-	fmt.Println(message);
+    fmt.Printf("SHA256: %x\n", hash.Sum(nil))
 }
