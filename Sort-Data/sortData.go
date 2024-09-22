125da203c262621f053de85dd6ee24469f266b06
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-09-23 02:42:31 +0600

    Added the insertion sort function

--- Sort-Data/sortData.go
+++ Sort-Data/sortData.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func readData() ([][]string, error) {
+	fileName, err := os.Open("data.csv");
+	if err != nil {
+		return nil, err;
+	}
+	defer fileName.Close();
+
+	rawData := csv.NewReader(fileName);
+	data, err :=rawData.ReadAll();
+	if err != nil {
+		return nil, err;
+	}
+
+	return data, nil
+}
+
+func buildArray(data [][]string) []int {
+	array := []int{};
+	for _, elem := range data {
+		num, _ := strconv.Atoi(elem[0]);
+		array = append(array, num);
+	} 
+	return array;
+}
+
+func insertionSort(array []int) []int {
+	for i := 0; i < len(array); i++ {
+		var key int = array[i];
+		var j int = i - 1;
+
+		for j >= 0 && array[j] > key {
+			array[j+1] = array[j];
+			j--;
+		}
+		array[j+1] = key;
+	}
+	return array;
+}
 
 func main() {
-	fmt.Println("Hello World");
+	data, err := readData();
+	if err != nil {
+		fmt.Println(err);
+		os.Exit(1);
+	}
+
+	var array []int = buildArray(data);
+	var sortedArray []int = insertionSort(array);
+
+	fmt.Println(sortedArray);
 }
