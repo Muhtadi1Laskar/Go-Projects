80bdca322be346616b9742b07eeb936cd506581a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-06 14:46:41 +0600

    Added Vingere Cipher

--- Ciphers/Vingere-Cipher/vingere.go
+++ Ciphers/Vingere-Cipher/vingere.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
+
+func vingereCipher(data string, key string, encrypt bool) string {
+	keyRunes := []rune(key)
+	textRunes := []rune(data)
+	result := make([]rune, len(textRunes))
+
+	for i, r := range textRunes {
+		if !unicode.IsLetter(r) {
+			result[i] = r
+			continue
+		}
+
+		shift := keyRunes[i%len(keyRunes)]
+		if unicode.IsUpper(shift) {
+			shift -= 'A'
+		} else {
+			shift -= 'a'
+		}
+
+		if !encrypt {
+			shift = -shift
+		}
+
+		if unicode.IsUpper(r) {
+			result[i] = 'A' + (r - 'A' + shift + 26) % 26
+		} else {
+			result[i] = 'a' + (r - 'a' + shift + 26) % 26
+		}
+	}
+	return string(result)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	plainText := "Hello"
+	key := "KEY"
+
+	encrypted := vingereCipher(plainText, key, true)
+	decrypted := vingereCipher(encrypted, key, false)
+
+	fmt.Println(encrypted)
+	fmt.Println(decrypted)
 }
