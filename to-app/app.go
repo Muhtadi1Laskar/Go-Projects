e8f303e5099fc2e738d019bec54aed7a79404a56
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-28 17:32:06 +0600

    Added List struct

--- to-app/app.go
+++ to-app/app.go
@@ -1,55 +1,22 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-type Task struct {
-	id int
-	description string
-	completed bool
+type Node struct {
+	value string
+	next *Node
 }
 
-var TaskList []Task;
-var taskIDCounter int = 1;
-
-func enterTask(description string, completed bool) {
-	newTask := Task{
-		id: taskIDCounter,
-		description: description,
-		completed: completed,
-	}
-	TaskList = append(TaskList, newTask);
-	taskIDCounter++;
+type LinkedList struct {
+	head *Node
+	tail *Node
+	length int
 }
 
-func findTask(numOfTask int) (Task, error) {
-	if numOfTask < 0 || numOfTask > len(TaskList) {
-		return Task{}, errors.New("The number of task is invalid");
-	}
-
-	for _, task := range TaskList {
-		if task.id == numOfTask {
-			return task, nil;
-		}
-	}
-	return Task{}, errors.New("Task not found");
+func NewLinkedList() *LinkedList {
+	return &LinkedList{ }
 }
-
 
 func main() {
-	for i := 0; i < 10; i++ {
-		str := fmt.Sprintf("This is the %d task", i+1);
-		enterTask(str, false);
-	}
-
-	task, err := findTask(1);
-	if err != nil {
-		fmt.Println(err);
-	} else {
-		fmt.Println(task);
-	}
-
-	fmt.Println(TaskList);
+	fmt.Println("Hello World")
 }
