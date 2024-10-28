a942c59df56ba3c46a773d5bf4f8c02ac3858a42
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-28 17:35:53 +0600

    Added Append() method

--- to-app/app.go
+++ to-app/app.go
@@ -21,6 +21,21 @@ func (list *LinkedList) isEmpty() bool {
 	return list.head == nil
 }
 
+func (list *LinkedList) Append(value string) {
+	newNode := &Node{ value: value }
+
+	if list.isEmpty() {
+		list.head = newNode
+		list.tail = newNode
+		list.length++
+		return
+	}
+	list.tail.next = newNode
+	list.tail = newNode
+	list.length++
+	return
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
