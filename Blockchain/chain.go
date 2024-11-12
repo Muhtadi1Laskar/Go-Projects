38c3d7eb47a69702595ec9749566e0cd3f36e19b
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-12 22:06:53 +0600

    Implemnted the create block method

--- Blockchain/chain.go
+++ Blockchain/chain.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"crypto/sha512"
 	"encoding/hex"
+	"fmt"
 )
+
+type Block struct {
+	index        int
+	timestamp    string
+	proof        int
+	previousHash string
+}
+
+type Chain struct {
+	chain []*Block
+}
+
+func (c *Chain) createBlock(proof int, previousHash string) *Block {
+	newBlock := &Block {
+		index: len(c.chain),
+		timestamp: "01/01/20002",
+		proof: proof,
+		previousHash: previousHash,
+	}
+	c.chain = append(c.chain, newBlock)
+
+	return newBlock
+}
 
 func sha521(text string) string {
 	byteMessage := []byte(text)
