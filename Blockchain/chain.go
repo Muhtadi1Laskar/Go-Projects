9b85d3eb5dfa696744af801f62a44fc6c8c1a898
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-12 22:25:47 +0600

    Added Hash method

--- Blockchain/chain.go
+++ Blockchain/chain.go
@@ -4,6 +4,8 @@ import (
 	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
+	"strconv"
+	"time"
 )
 
 type Block struct {
@@ -18,15 +20,25 @@ type Chain struct {
 }
 
 func (c *Chain) createBlock(proof int, previousHash string) *Block {
-	newBlock := &Block {
-		index: len(c.chain),
-		timestamp: "01/01/20002",
-		proof: proof,
+	newBlock := &Block{
+		index:        len(c.chain),
+		timestamp:    time.Now().Format("2006-01-02 15:04:05"),
+		proof:        proof,
 		previousHash: previousHash,
 	}
 	c.chain = append(c.chain, newBlock)
 
 	return newBlock
+}
+
+func (c *Chain) getPrevBlock() *Block {
+	return c.chain[len(c.chain)-1]
+}
+
+func (c *Chain) hash(block *Block) string {
+	record := strconv.Itoa(block.index) + block.timestamp + strconv.Itoa(block.proof) + block.previousHash
+	hash := sha521(record)
+	return hash
 }
 
 func sha521(text string) string {
@@ -41,5 +53,8 @@ func sha521(text string) string {
 }
 
 func main() {
-	fmt.Println(sha521("Hello World"))
+	chain := &Chain{}
+	block := chain.createBlock(12345, "0")
+
+	fmt.Printf("%+v\n", block)
 }
