a0c78b72303c003614f23f3aef56c90c8dbae6e9
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-16 18:18:23 +0600

    Formatted the code

--- quiz-app/app.go
+++ quiz-app/app.go
@@ -13,52 +13,52 @@ type problem struct {
 }
 
 func main() {
-	csvFileName := flag.String("cvs", "problems.csv", "a csv file in the format of 'question, answer'");
-	flag.Parse();
+	csvFileName := flag.String("cvs", "problems.csv", "a csv file in the format of 'question, answer'")
+	flag.Parse()
 
-	file, err := os.Open(*csvFileName);
-
-	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName));
-		os.Exit(0);
-	}
-
-	r := csv.NewReader(file);
-	lines, err := r.ReadAll();
+	file, err := os.Open(*csvFileName)
 
 	if err != nil {
-		exit("Failed to parse the csv file");
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName))
+		os.Exit(0)
 	}
 
-	problems := parseLines(lines);
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+
+	if err != nil {
+		exit("Failed to parse the csv file")
+	}
+
+	problems := parseLines(lines)
 	correctScore := 0
 
 	for idx, elem := range problems {
-		fmt.Printf("Problems # %d: %s = \n", idx+1, elem.q);
+		fmt.Printf("Problems # %d: %s = \n", idx+1, elem.q)
 
-		var answer string;
-		fmt.Scanf("%s\n", &answer);
+		var answer string
+		fmt.Scanf("%s\n", &answer)
 
 		if answer == elem.a {
-			correctScore += 1;
+			correctScore += 1
 		}
 	}
-	fmt.Printf("You have %v correct answers out of %v", correctScore, len(problems));
+	fmt.Printf("You have %v correct answers out of %v", correctScore, len(problems))
 
 }
 
 func parseLines(lines [][]string) []problem {
-	ret := make([]problem, len(lines));
+	ret := make([]problem, len(lines))
 	for idx, line := range lines {
 		ret[idx] = problem{
 			q: line[0],
 			a: line[1],
 		}
 	}
-	return ret;
+	return ret
 }
 
 func exit(message string) {
-	fmt.Println(message);
-	os.Exit(1);
+	fmt.Println(message)
+	os.Exit(1)
 }
