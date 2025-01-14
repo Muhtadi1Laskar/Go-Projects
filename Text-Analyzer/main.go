42b2f898de5898df6ccbb072f84d28c7aab39abf
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-01-15 00:00:50 +0600

    Added the function to calculate the word frequency

--- Text-Analyzer/main.go
+++ Text-Analyzer/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
-	"bufio"
+	"unicode"
 )
-
 
 func countWords(text string) int {
 	return len(strings.Fields(text))
@@ -17,7 +17,7 @@ func countCharacters(text string) int {
 
 	for _, char := range text {
 		if !isWhiteSpace(char) {
-			count++;
+			count++
 		}
 	}
 	return count
@@ -25,7 +25,7 @@ func countCharacters(text string) int {
 
 func countSentences(text string) int {
 	count := 0
-	sentenceDelimeters := map[rune]bool {
+	sentenceDelimeters := map[rune]bool{
 		'.': true,
 		'!': true,
 		'?': true,
@@ -52,17 +52,44 @@ func isWhiteSpace(char rune) bool {
 func count(text, operation string) int {
 	text = strings.TrimSpace(text)
 	operation = strings.ToLower(operation)
-	
+
 	switch operation {
-		case "word-count":
-			return countWords(text)
-		case "character-count":
-			return countCharacters(text)
-		case "sentence-count":
-			return countSentences(text)
-		default:
-			return -1
+	case "word-count":
+		return countWords(text)
+	case "character-count":
+		return countCharacters(text)
+	case "sentence-count":
+		return countSentences(text)
+	default:
+		return -1
 	}
+}
+
+func handlePunctuation(text string) string {
+	var builder strings.Builder
+
+	for _, word := range text {
+		if word == '-' || word == '\'' ||!unicode.IsPunct(word) {
+			builder.WriteRune(word)
+		}
+	}
+	return builder.String()
+}
+
+func calculateFreq(text string) map[string]int {
+	formattedStr := handlePunctuation(text)
+	words := tokenize(formattedStr)
+	frequency := make(map[string]int)
+
+	for _, word := range words {
+		frequency[word]++
+	}
+	return frequency
+}
+
+func tokenize(text string) []string {
+	text = strings.ToLower(text)
+	return strings.Fields(text)
 }
 
 func readFile(PATH string) (string, error) {
@@ -88,10 +115,15 @@ func readFile(PATH string) (string, error) {
 }
 
 func main() {
-	var path string = "C:/Users/SYSNET/OneDrive/Documents/Coding/Golang/projects/Plagrism-Checker/document1.txt"
+	var path string = "C:/Users/SYSNET/OneDrive/Documents/Coding/Golang/projects/Text-Analyzer/text.txt"
 	data, _ := readFile(path)
+	freq := calculateFreq(data)
 
 	fmt.Println("Word Count:", count(data, "word-count"))
 	fmt.Println("Character Count:", count(data, "character-count"))
 	fmt.Println("Sentence Count:", count(data, "sentence-count"))
+
+	for key, value := range freq {
+		fmt.Println(key, value)
+	}
 }
