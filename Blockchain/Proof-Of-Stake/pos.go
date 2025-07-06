f3321f11d41ceec9818a474b30fda73a0dd730f3
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-06 16:45:36 +0600

    Added block and chain interface for proof of stake implementation

--- Blockchain/Proof-Of-Stake/pos.go
+++ Blockchain/Proof-Of-Stake/pos.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+type Block struct {
+	index        int
+	timeStamp    string
+	data         string
+	previousHash string
+	validator    string
+	hash         string
+}
+
+type Chain struct {
+	chain     []*Block
+	validator map[string]int
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
