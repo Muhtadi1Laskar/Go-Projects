07ebb96a1618c7c3856ba6c1de96af051bf4509a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-06 16:51:09 +0600

    Added function to hash blocks

--- Blockchain/Proof-Of-Stake/pos.go
+++ Blockchain/Proof-Of-Stake/pos.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/sha512"
+	"encoding/hex"
 	"fmt"
+	"strconv"
 )
 
 type Block struct {
@@ -16,6 +19,23 @@ type Block struct {
 type Chain struct {
 	chain     []*Block
 	validator map[string]int
+}
+
+func (chain *Chain) hashBlock(block *Block) string {
+	record := strconv.Itoa(block.index) + block.data + block.timeStamp + block.previousHash
+	hash := computeHash(record)
+	return hash
+}
+
+func computeHash(data string) string {
+	byteMessage := []byte(data)
+	hash := sha512.New()
+	hash.Write(byteMessage)
+
+	hashedBytes := hash.Sum(nil)
+	encodedStr := hex.EncodeToString(hashedBytes)
+
+	return encodedStr
 }
 
 func main() {
