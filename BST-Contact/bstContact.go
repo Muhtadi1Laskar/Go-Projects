645f91f21cf4af5b98d3aa34572401a5a9c0dab5
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-10 16:42:19 +0600

    Added the insert() method for contact bst

--- BST-Contact/bstContact.go
+++ BST-Contact/bstContact.go
@@ -4,6 +4,64 @@ import (
 	"fmt"
 )
 
+type Node struct {
+	left  *Node
+	right *Node
+	value map[string]string
+}
+
+type Tree struct {
+	root *Node
+}
+
+func newTree() *Tree {
+	return &Tree{root: nil}
+}
+
+func (bst *Tree) insert(data map[string]string) {
+	if bst.root == nil {
+		bst.root = &Node{ value: data }
+	} else {
+		bst.insertHelper(bst.root, data)
+	}
+}
+
+func (bst *Tree) insertHelper(node *Node, data map[string]string) {
+	if node.value["name"] < data["name"] {
+		if node.left == nil {
+			node.left = &Node{value: data}
+		} else {
+			bst.insertHelper(node.left, data)
+		}
+	} else {
+		if node.right == nil {
+			node.right = &Node{value: data}
+		} else {
+			bst.insertHelper(node.right, data)
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var arr []map[string]string = []map[string]string{
+		{
+			"name":   "Luffy",
+			"number": "01775900737",
+		},
+		{
+			"name":   "Zoro",
+			"number": "01338577464",
+		},
+		{
+			"name":   "Sanji",
+			"number": "55337866546",
+		},
+	}
+	tree := newTree()
+
+	for _, data := range arr {
+		tree.insert(data)
+	}
+
+	fmt.Println(tree.root)
 }
