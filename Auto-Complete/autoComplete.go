e9a48725e12d61cc215de528dfbd3eec3438e0d4
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-10 19:25:39 +0600

    Added the iterative DFS for auto complete BST

--- Auto-Complete/autoComplete.go
+++ Auto-Complete/autoComplete.go
@@ -42,13 +42,33 @@ func (bst *Tree) insertHelper(currentNode *Node, data string) {
 	}
 }
 
+func (bst *Tree) iterativeDFS() []string {
+	var result []string
+	var stack []*Node
+	var currentNode = bst.root
+
+	for currentNode != nil || len(stack) > 0 {
+		for currentNode != nil {
+			stack = append(stack, currentNode)
+			currentNode = currentNode.left
+		}
+		currentNode = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		result = append(result, currentNode.value)
+
+		currentNode = currentNode.right
+	}
+	return result
+}
+
 func main() {
 	var tree = new_tree()
-	var words []string = []string{"apple", "app", "apricot", "banana", "ball", "cat", "a", "czech", "carrot"}
+	var words []string = []string{"apple", "app", "apricot", "xoy", "banana", "ball", "cat", "a", "czech", "carrot"}
 
 	for _, word := range words {
 		tree.insert(word)
 	}
 
-	fmt.Println(tree.root.value)
+	fmt.Println(tree.iterativeDFS())
 }
