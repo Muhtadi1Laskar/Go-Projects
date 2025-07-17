d87da6614a344b0eaccc526b380c8acc5c077615
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-17 22:30:18 +0600

    Implemented the function to read word list

--- BIP-39/main.go
+++ BIP-39/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
+func readWordList(filePath string) []string {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Failed to read wordlist: %v", err)
+	}
+	return strings.Split(strings.TrimSpace(string(data)), "\n")
+}
+
+func hashBytes(data []byte) []byte {
+	hash := sha256.New()
+	hash.Write(data)
+
+	hashedBytes := hash.Sum(nil)
+	return hashedBytes
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var total int = 16
+
+	var randomBytes []byte = make([]byte, total)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		log.Fatal("Error while generating random number", err)
+	}
+
+	hashedBytes := hashBytes(randomBytes)
+	firstByte := randomBytes[0]
+	first4Bits := firstByte >> 4
+
+	fmt.Printf("Generated Random Bytes (hex): %x\n", randomBytes)
+	fmt.Printf("Generated Random Bytes (raw): %v\n", randomBytes)
+	fmt.Printf("Generated Random Bytes (hashed raw): %v\n", hashedBytes)
+	fmt.Println(hex.EncodeToString(hashedBytes))
+	fmt.Println(first4Bits)
 }
