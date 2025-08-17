ae236ff88165b553b166e7bdcf9edb14d266fffb
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-08-17 13:12:23 +0600

    Added the function to read the sample from signal

--- Fourier-Transformation/go.mod
+++ Fourier-Transformation/go.mod
@@ -4,5 +4,7 @@ go 1.24.3
 
 require (
 	github.com/faiface/beep v1.1.0 // indirect
+	github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	gonum.org/v1/gonum v0.16.0 // indirect
 )
--- Fourier-Transformation/main.go
+++ Fourier-Transformation/main.go
@@ -11,7 +11,10 @@ import (
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/wav"
+	"github.com/mjibson/go-dsp/fft"
 )
+
+const N = 4096
 
 func getFilePath() string {
 	_, filename, _, _ := runtime.Caller(0)
@@ -19,7 +22,7 @@ func getFilePath() string {
 	return filepath.Join(dir, "./sounds/sample.wav")
 }
 
-func Dft(signal []float32) []complex128 {
+func Dft(signal []float64) []complex128 {
 	var N int = len(signal)
 	var result = make([]complex128, N)
 
@@ -52,6 +55,19 @@ func decodeAudio(f *os.File) (beep.StreamSeekCloser, beep.Format) {
 	return streamer, format
 }
 
+func readSample(streamer beep.StreamSeekCloser) []float64 {
+	samples := make([]float64, N)
+	buf := make([][2]float64, 1)
+	for i := 0; i < N; i++ {
+		sample, ok := streamer.Stream(buf)
+		if !ok || sample == 0 {
+			break
+		}
+		samples[i] = buf[0][0]
+	}
+	return samples
+}
+
 func main() {
 	path := getFilePath()
 	f := readAudioFile(path)
@@ -60,6 +76,14 @@ func main() {
 	streamer, format := decodeAudio(f)
 	defer streamer.Close()
 
-	fmt.Println(streamer)
-	fmt.Println(format)
+	samples := readSample(streamer)
+	fourier := Dft(samples)
+
+	fourier2 := fft.FFTReal(samples)
+
+	// Use the Dft result directly
+	fmt.Printf("%T\n", streamer)
+	fmt.Printf("%T\n", format)
+	fmt.Println(fourier[0])
+	fmt.Println(fourier2[0])
 }
