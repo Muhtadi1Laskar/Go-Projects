d4747de0e9f3d1f48868074f4e127ba1b1f09611
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-08-19 16:37:23 +0600

    Added the audio encoder function

--- Audio-Cutter/main.go
+++ Audio-Cutter/main.go
@@ -2,8 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/faiface/beep/mp3"
 )
 
+func getFilePath(folderName string) string {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+	return filepath.Join(dir, folderName)
+}
+
+func EncodeAudio(filepath string, start, end int) []float64 {
+	f, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	streamer, _, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	var samples []float64
+	buf := make([][2]float64, 1)
+	for {
+		sample, ok := streamer.Stream(buf)
+		if !ok || sample == 0 {
+			break
+		}
+		samples = append(samples, float64(buf[0][0]))
+	}
+	samplesPerSecond := 44100
+	startIndex := start * samplesPerSecond
+	endIndex := end * samplesPerSecond
+	cutSamples := samples[startIndex:endIndex]
+
+	return cutSamples
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var filePath string = getFilePath("./Audio/sample.mp3")
+	var startTime int = 10
+	var endTime int = 70
+	samples := EncodeAudio(filePath, startTime, endTime)
+
+	fmt.Println(len(samples))
 }
