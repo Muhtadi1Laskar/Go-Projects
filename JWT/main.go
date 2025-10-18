f1e3783172845457af2a7542c4be11e111c63333
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-19 01:19:01 +0600

    Added the base64urlEncode function

--- JWT/main.go
+++ JWT/main.go
@@ -1,8 +1,26 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"strings"
+	"time"
 )
+
+const (
+	JWT_SECRET = "This_is_a_secret_KEY"
+	ALGORTIHM = "HS256"
+)
+
+func base64urlEncode(data []byte) string {
+	str := base64.URLEncoding.EncodeToString(data)
+	return strings.TrimRight(str, "=")
+}
+
 
 func main() {
 	fmt.Println("Hello World")
