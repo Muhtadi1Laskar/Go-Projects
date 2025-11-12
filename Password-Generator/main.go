7ffa70776fd6d47ee30e6b55f061aabf26009c76
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-12 06:48:43 +0600

    Started the password generation project

--- Password-Generator/main.go
+++ Password-Generator/main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strings"
 )
 
+var PASSWORD_TYPES = map[string]string{
+	"lower": "abcdefghijklmnopqrstuvwxyz",
+	"upper": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	"digits": "0123456789",
+	"symbols": "!@#$%^&*()-_=+[]{}|;:,.<>?/",
+}
+
+func getRandomChar(chars string) string {
+	return string(chars[rand.Intn(len(chars))])
+}
+
+func generatePassword(length int, types []string) string {
+	if length < 4 {
+		return "Password must be greater than 4"
+	}
+
+	var result []string = make([]string, length)
+	for i := range types {
+		c := PASSWORD_TYPES[types[i]]
+		if len(c) == 0 {
+			continue
+		}
+		result = append(result, getRandomChar(c))
+	}
+
+	var charPool string = ""
+	for i := range types {
+		charPool += PASSWORD_TYPES[types[i]]
+	}
+
+	for len(result) < length {
+		result = append(result, getRandomChar(charPool))
+	}
+
+	return strings.Join(result, "")
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var types []string = []string{"lower", "digits", "upper"}
+
+	fmt.Println(generatePassword(10, types))
 }
