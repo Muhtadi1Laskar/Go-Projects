25a15a1e218ec7341e949902f0fe333d1019942d
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-27 14:27:44 +0600

    Added the function to hash files

--- Check-Duplicate-Files/main.go
+++ Check-Duplicate-Files/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io"
+	"os"
 )
 
+func hashFile(path string) ([32]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return [32]byte{}, err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return [32]byte{}, err
+	}
+
+	var sum [32]byte
+	copy(sum[:], h.Sum(nil))
+	return sum, nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var filePathString string = "C:/Users/laska/Downloads/Pictures/JPG Format/IMG_3291 12x16.jpg"
+	fmt.Println(hashFile(filePathString))
+
 }
