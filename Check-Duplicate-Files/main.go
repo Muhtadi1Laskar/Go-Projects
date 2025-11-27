5b156e038c2e1d470beb0f800a33173ba8749f40
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-11-27 14:40:06 +0600

    Completed the Check Duplicates Files project

--- Check-Duplicate-Files/main.go
+++ Check-Duplicate-Files/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 func hashFile(path string) ([32]byte, error) {
@@ -24,8 +25,62 @@ func hashFile(path string) ([32]byte, error) {
 	return sum, nil
 }
 
+func findDuplicateFiles(root string) (map[[32]byte][]string, error) {
+	sizeGroups := make(map[int64][]string)
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		sizeGroups[info.Size()] = append(sizeGroups[info.Size()], path)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	duplicates := make(map[[32]byte][]string)
+
+	for _, files := range sizeGroups {
+		if len(files) < 2 {
+			continue
+		}
+		for _, file := range files {
+			hash, err := hashFile(file)
+			if err != nil {
+				return nil, err
+			}
+			duplicates[hash] = append(duplicates[hash], file)
+		}
+	}
+
+	result := make(map[[32]byte][]string)
+	for hash, paths := range duplicates {
+		if len(paths) > 1 {
+			result[hash] = paths
+		}
+	}
+
+	return result, nil
+}
+
 func main() {
-	var filePathString string = "C:/Users/laska/Downloads/Pictures/JPG Format/IMG_3291 12x16.jpg"
-	fmt.Println(hashFile(filePathString))
+	root := "C:/Users/laska/Videos"
+
+	dups, err := findDuplicateFiles(root)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Duplicate Files:")
+	for hash, files := range dups {
+		fmt.Printf("\nHash: %x\n", hash)
+		for _, f := range files {
+			fmt.Println("  -", f)
+		}
+	}
 
 }
